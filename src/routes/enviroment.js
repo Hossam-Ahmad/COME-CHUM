c1c94285e3828b2e4e70e31f01aa05de1aff5c51
Hossam-Ahmad <hossam.ahmad.programmer@gmail.com>
2020-04-24 17:19:46 +0200

    updates

--- src/routes/enviroment.js
+++ src/routes/enviroment.js
@@ -7,11 +7,12 @@ const mysql = require('mysql');
 //     database : 'travel'
 // });
 
+var connection = mysql.createConnection({
+    host     : 'mysql://bad024e7c20c55:f1fb3c46@us-cdbr-iron-east-01.cleardb.net/heroku_f0cb29b5b6f62bb?reconnect=true',
+    user     : 'bad024e7c20c55',
+    password : 'f1fb3c46',
+    database : 'heroku_f0cb29b5b6f62bb'
+});
+
 module.exports = connection;
 
-var connection = mysql.createConnection({
-  host     : 'mysql://bad024e7c20c55:f1fb3c46@us-cdbr-iron-east-01.cleardb.net/heroku_f0cb29b5b6f62bb?reconnect=true',
-  user     : 'bad024e7c20c55',
-  password : 'f1fb3c46',
-  database : 'heroku_f0cb29b5b6f62bb'
-});
