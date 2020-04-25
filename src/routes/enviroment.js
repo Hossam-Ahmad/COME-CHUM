d0d438b70b1949e37db299b46f87f916a0c77848
Hossam-Ahmad <hossam.ahmad.programmer@gmail.com>
2020-04-25 23:36:07 +0200

    changes

--- src/routes/enviroment.js
+++ src/routes/enviroment.js
@@ -1,20 +1,20 @@
 const mysql = require('mysql');
 
-var connection = mysql.createPool({
-    connectionLimit : 10, // default = 10
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'travel'
-});
-
 // var connection = mysql.createPool({
 //     connectionLimit : 10, // default = 10
-//     host     : 'us-cdbr-iron-east-01.cleardb.net',
-//     user     : 'bad024e7c20c55',
-//     password : 'f1fb3c46',
-//     database : 'heroku_f0cb29b5b6f62bb'
+//     host     : 'localhost',
+//     user     : 'root',
+//     password : '',
+//     database : 'travel'
 // });
+
+var connection = mysql.createPool({
+    connectionLimit : 10, // default = 10
+    host     : 'us-cdbr-iron-east-01.cleardb.net',
+    user     : 'bad024e7c20c55',
+    password : 'f1fb3c46',
+    database : 'heroku_f0cb29b5b6f62bb'
+});
 
 module.exports = connection;
 
