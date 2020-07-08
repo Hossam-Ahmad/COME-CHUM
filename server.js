e676387d85e8a6b50d755442e60300ec67bb574c
Hossam-Ahmad <hossam.ahmad.programmer@gmail.com>
2020-07-08 13:50:59 +0200

    updates

--- server.js
+++ server.js
@@ -66,7 +66,8 @@ app.get('/googlec2d96c9b2c4b2245.html', (request, response) => {
 });
 
 app.get('/*', (request, response) => {
-    response.sendFile(__dirname + '/dist/travel-app/index.html');
+    // response.sendFile(__dirname + '/dist/travel-app/index.html');
+    response.sendFile('/dist/travel-app/index.html');
 });
 
 const server = http.createServer(app);
