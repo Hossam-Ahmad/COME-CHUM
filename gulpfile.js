e8ab4ad4686abe463244f7f28f6336068475eb31
Hossam-Ahmad <hossam.ahmad.programmer@gmail.com>
2020-07-15 13:47:05 +0200

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var minify = require('gulp-minify');
 
 gulp.task('minify', function() {
-  return gulp.src(['./dist/travel-app/*.js'])
+  return gulp.src(['./dist/travel-app/*.js'], {base: './'})
       .pipe(minify())
       .pipe(gulp.dest('./dist/travel-app'));
 });
